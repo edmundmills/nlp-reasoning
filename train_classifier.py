bd5757319c3585705921427125c5e667f078b78a
Edmund Mills <edmund.martin.mills@gmail.com>
2021-12-14 08:41:39 -0700

    Add eval and early stopping

--- train_classifier.py
+++ train_classifier.py
@@ -1,3 +1,4 @@
+from collections import deque
 import json
 import os
 import random
@@ -5,7 +6,7 @@ from typing import Dict, List, Tuple
 
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, random_split, DataLoader
+from torch.utils.data import TensorDataset, random_split, DataLoader, RandomSampler, SequentialSampler
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
 import wandb
 
@@ -60,13 +61,19 @@ def train(model, train_dataset, test_dataset, **params):
     dataloader = DataLoader(train_dataset,
                             batch_size=params['batch_size'],
                             num_workers=4,
-                            drop_last=True)
+                            drop_last=True,
+                            sampler=RandomSampler(train_dataset))
     
     optimizer = AdamW(model.parameters(),
                       lr=params['lr'],
                       eps=1e-8)
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.train()
+
+    running_eval_len = 5
+    running_eval = deque([float('Inf')]*running_eval_len, maxlen=running_eval_len)
+    stop = False
 
     for epoch in range(1, params['epochs'] + 1):
         print(f'Starting Epoch {epoch}')
@@ -78,8 +85,8 @@ def train(model, train_dataset, test_dataset, **params):
             labels = labels.to(device)
             
             outputs = model(input_ids,  
-                                  attention_mask=attention_masks, 
-                                  labels=labels)
+                            attention_mask=attention_masks, 
+                            labels=labels)
             loss = outputs.loss
 
             optimizer.zero_grad()
@@ -92,23 +99,66 @@ def train(model, train_dataset, test_dataset, **params):
             total_train_loss += loss
 
             if step % 50 == 0:
+                prev_avg_eval_loss = sum(running_eval) / len(running_eval)
                 print(f'Epoch: {epoch}\tStep: {step} \tLoss: {loss:.3f}')
-        
+                eval_metrics = eval(model, test_dataset, eval_samples=300, **params)
+                print(eval_metrics)
+                running_eval.append(eval_metrics['Eval Loss'])
+                avg_eval_loss = sum(running_eval) / len(running_eval)
+                if avg_eval_loss > prev_avg_eval_loss:
+                    stop = 'Eval Loss Increasing, Stopping Early'
+
+            if stop:
+                print(stop)
+                eval_metrics = eval(model, test_dataset, **params)
+                print(eval_metrics)
+                return
+
         avg_train_loss = total_train_loss / len(dataloader)
         print(f'Epoch {epoch}\t\tAvg Training Loss: {avg_train_loss:.3f}')
-        
+        eval_metrics = eval(model, test_dataset, **params)
+        print(eval_metrics)
 
 
-def eval(model, test_dataset, **params):
+def eval(model, test_dataset, eval_samples=None, **params):
+    sampler = RandomSampler if eval_samples is not None else SequentialSampler
     dataloader = DataLoader(test_dataset,
                             batch_size=params['batch_size'],
                             num_workers=4,
-                            drop_last=True)
+                            drop_last=True,
+                            sampler=sampler(test_dataset))
 
-    for input_ids, attention_masks, labels in dataloader:
-        pass
+    eval_samples = eval_samples or len(dataloader)
 
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.eval()
 
+    total_eval_loss = 0
+    accuracies = []
+    for step, (input_ids, attention_masks, labels) in enumerate(dataloader):
+        input_ids = input_ids.to(device)
+        attention_masks = attention_masks.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            outputs = model(input_ids,  
+                            attention_mask=attention_masks, 
+                            labels=labels)
+        total_eval_loss += outputs.loss.item()
+        logits = outputs.logits.cpu().numpy()
+        labels = labels.cpu().numpy().flatten()
+        predicted = np.argmax(logits, axis=1).flatten()
+        accuracy = np.sum(predicted == labels) / len(labels)
+        accuracies.append(accuracy)
+        if (step + 1) * params['batch_size'] > eval_samples:
+            break
+
+    avg_eval_loss = total_eval_loss / len(accuracies)
+    avg_accuracy = sum(accuracies) / len(accuracies)
+    metrics = {'Eval Loss': avg_eval_loss, 'Eval Accuracy': avg_accuracy}
+    wandb.log(metrics)
+    model.train()
+    return metrics
 
 if __name__ == '__main__':
     params = {
@@ -126,7 +176,6 @@ if __name__ == '__main__':
     )
 
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
 
     random.seed(params['seed'])
     np.random.seed(params['seed'])
