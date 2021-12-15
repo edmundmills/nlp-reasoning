3f201f7c854ce0cad7ef131e53935f216b1e4993
Edmund Mills <edmund.martin.mills@gmail.com>
2021-12-15 08:33:04 -0700

    Distinguish classifier and generator datasets

--- dataset.py
+++ dataset.py
@@ -20,22 +20,66 @@ def clean_text(text):
         return text
 
 class Dataset:
-    def __init__(self, dataset=None):
-        self.dataset = dataset
-        if dataset is not None:
+    def __init__(self, dataset_name=None):
+        self.dataset_name = dataset_name
+        if dataset_name is not None:
             print('Loading Data...')
-            self.data = self._load_dataset(dataset)
+            self.data = self._load_dataset(dataset_name)
             print(f'{len(self)} samples loaded')
         else:
             self.data = None
         self.tokenizer = Tokenizer()
 
-    def _load_dataset(self, dataset):
-        if dataset  == 'sarcasm_headlines':
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def train_test_split(self, ratio):
+        train_size = int(len(self)*ratio)
+        train_dataset = self.__class__()
+        train_dataset.data = self.data[:train_size]
+        test_dataset = self.__class__()
+        test_dataset.data = self.data[train_size:]
+        print(f'{len(train_dataset)} training samples')
+        print(f'{len(test_dataset)} test samples')
+        return train_dataset, test_dataset
+
+class ClassifierDataset(Dataset):
+    def _load_dataset(self, dataset_name):
+        if dataset_name  == 'sarcasm_headlines':
             data = list(parse_data('data/Sarcasm_Headlines_Dataset_v2.json'))
             positive = sum(sample['is_sarcastic'] for sample in self.data)
             print(f'{positive/len(self)*100:.1f}% sarcastic samples')
-        elif dataset == 'winning_arguments':
+        else:
+            raise ValueError
+        return data
+
+    def to_tokenized_tensors(self, model:str) -> TensorDataset:
+        input_ids = []
+        attention_masks = []
+        labels = []
+
+        print('Tokenizing data...')
+
+        for sample in self.data:
+            encoded_dict = self.tokenizer.encode(sample, model)
+            input_ids.append(encoded_dict['input_ids'])
+            attention_masks.append(encoded_dict['attention_mask'])
+            labels.append(sample['is_sarcastic'])
+
+        input_ids = torch.cat(input_ids, dim=0)
+        attention_masks = torch.cat(attention_masks, dim=0)
+        labels = torch.tensor(labels)
+        tensor_dataset = TensorDataset(input_ids, attention_masks, labels)
+        print('Data tokenized.')
+        return tensor_dataset
+
+
+class GeneratorDataset(Dataset):
+    def _load_dataset(self, dataset):
+        if dataset == 'winning_arguments':
             data_dict = {}
             data = []
             print('Building reply lookup dict...')
@@ -60,23 +104,6 @@ class Dataset:
         else:
             raise ValueError
         return data
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx]
-
-    def train_test_split(self, ratio):
-        train_size = int(len(self)*ratio)
-        test_size = len(self) - train_size
-        train_dataset = Dataset()
-        train_dataset.data = self.data[:train_size]
-        test_dataset = Dataset()
-        test_dataset.data = self.data[train_size:]
-        print(f'{len(train_dataset)} training samples')
-        print(f'{len(test_dataset)} test samples')
-        return train_dataset, test_dataset
 
     def to_tokenized_tensors(self, model:str) -> TensorDataset:
         input_ids = []
